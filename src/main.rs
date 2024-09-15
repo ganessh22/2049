2f01327816ca729cf9be5cc17d1c964af589f22c
ganessh22 <ganesh@hakimo.ai>
2024-09-15 15:44:52 +0530

    dioxus template working

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,52 @@
-use dioxus::prelude::*;
+#![allow(non_snake_case)]
 
-fn main() {
-    dioxus_web::launch(app);
+use dioxus::prelude::*;
+use dioxus_logger::tracing::{info, Level};
+
+#[derive(Clone, Routable, Debug, PartialEq)]
+enum Route {
+    #[route("/")]
+    Home {},
+    #[route("/blog/:id")]
+    Blog { id: i32 },
 }
 
-fn app(cx: Scope) -> Element {
-    cx.render(rsx! {
-        style { [include_str!("styles.css")] }
-        div { class: "container",
-            div { class: "grid",
-                (0..16).map(|_| rsx!(
-                    div { class: "cell" }
-                ))
-            }
+fn main() {
+    // Init logger
+    dioxus_logger::init(Level::INFO).expect("failed to init logger");
+    info!("starting app");
+    launch(App);
+}
+
+fn App() -> Element {
+    rsx! {
+        Router::<Route> {}
+    }
+}
+
+#[component]
+fn Blog(id: i32) -> Element {
+    rsx! {
+        Link { to: Route::Home {}, "Go to counter" }
+        "Blog post {id}"
+    }
+}
+
+#[component]
+fn Home() -> Element {
+    let mut count = use_signal(|| 0);
+
+    rsx! {
+        Link {
+            to: Route::Blog {
+                id: count()
+            },
+            "Go to blog"
         }
-    })
+        div {
+            h1 { "High-Five counter: {count}" }
+            button { onclick: move |_| count += 1, "Up high!" }
+            button { onclick: move |_| count -= 1, "Down low!" }
+        }
+    }
 }
